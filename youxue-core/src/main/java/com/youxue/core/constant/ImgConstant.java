b6a53e027d1718a434687801c6f6e91b8235dc9c
Masterwind <zhangwei2014@corp.netease.com>
2016-10-21 18:00:30 +0800

    图片服务器

--- youxue-core/src/main/java/com/youxue/core/constant/ImgConstant.java
+++ youxue-core/src/main/java/com/youxue/core/constant/ImgConstant.java
@@ -4,8 +4,14 @@ public class ImgConstant
 {
 	public static final String IMG_BASE_URL = "/";
 
-	public static String getImgUrl(String imgType, String imgName)
+	//	public static String getImgUrl(String imgUrl, String imgType, String imgName)
+	//	{
+	//		return IMG_BASE_URL + "/" + imgUrl + "/" + imgName + "." + imgType;
+	//	}
+
+	public static String getImgUrl(String imgUrl, String imgType, String imgName)
 	{
-		return IMG_BASE_URL + "/" + imgType + "/" + imgName;
+		String filePath = "D:\\img\\" + imgUrl + "\\" + imgName + "." + imgType;
+		return filePath;
 	}
 }
--- youxue-img/src/main/java/com/youxue/img/controller/ImgController.java
+++ youxue-img/src/main/java/com/youxue/img/controller/ImgController.java
@@ -12,22 +12,46 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.youxue.core.constant.ImgConstant;
 
 @Controller
 public class ImgController
 {
-	@RequestMapping("/img/{imgType}/{imgName}.png")
-	public void getImage(HttpServletRequest request, HttpServletResponse response,
-			@PathVariable("imgType") String imgType, @PathVariable("imgName") String imgName) throws Exception
+	@RequestMapping("/index.html")
+	@ResponseBody
+	public String index(HttpServletRequest request, HttpServletResponse response)
 	{
-		if (StringUtils.isBlank(imgName) || StringUtils.isBlank(imgType))
+		return "hello world";
+	}
+
+	/**
+	 * @param request
+	 * @param response
+	 * @param imgUrl 文件按功能划分的路径： 如缩略图、商品图等
+	 * @param imgType 文件按后缀类型划分的路径
+	 * @param imgName 文件名
+	 * @throws Exception
+	 */
+	@RequestMapping("/img/{imgUrl}/{imgType}/{imgName}.png")
+	public void getImage(HttpServletRequest request, HttpServletResponse response,
+			@PathVariable("imgUrl") String imgUrl, @PathVariable("imgType") String imgType,
+			@PathVariable("imgName") String imgName) throws Exception
+	{
+		if (StringUtils.isBlank(imgUrl) || StringUtils.isBlank(imgName) || StringUtils.isBlank(imgType))
 		{
 			return;
 		}
-		BufferedImage image = ImageIO.read(new File(ImgConstant.getImgUrl(imgType, imgName)));
-		response.setContentType("image/png");
+		String filePath = ImgConstant.getImgUrl(imgUrl, imgType, imgName);
+		File file = new File(filePath);
+		if (file == null || !file.exists())
+		{
+			System.out.println("file not exist,filePath:" + filePath);
+			return;
+		}
+		BufferedImage image = ImageIO.read(file);
+		response.setContentType("image/" + imgType);
 		OutputStream os = response.getOutputStream();
 		ImageIO.write(image, "png", os);
 	}

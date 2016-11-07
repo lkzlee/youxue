f8c142d291f79eb7cbd8e35314f05a0393a05aba
liyongchao <liyongchao@corp.netease.com>
2016-11-07 16:35:52 +0800

    项目构建添加数据源

---
+++ youxue-core/src/main/java/com/youxue/core/dao/BaseDao.java
@@ -0,0 +1,39 @@
+package com.youxue.core.dao;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.apache.ibatis.session.RowBounds;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.youxue.core.vo.Page;
+
+@Repository("baseDao")
+public class BaseDao
+{
+	@Resource(name = "sqlSessionTemplate")
+	public SqlSessionTemplate sqlSessionTemplate;
+
+	public <T> Page<T> getPageList(Page<T> page, String listSql, String countSql, Object param)
+	{
+		return getPageList(page, listSql, countSql, param, sqlSessionTemplate);
+	}
+
+	public <T> Page<T> getPageList(Page<T> page, String listSql, String countSql, Object param,
+			SqlSessionTemplate sqlSessionTemplate)
+	{
+		int skipResults = (page.getPageNo() - 1) * page.getPageSize();
+		if (page.getPageNo() < 1)
+		{
+			skipResults = 0;
+		}
+		int maxResults = page.getPageSize();
+		List<T> resultList = sqlSessionTemplate.selectList(listSql, param, new RowBounds(skipResults, maxResults));
+		page.setResult(resultList);
+		int totalCount = sqlSessionTemplate.selectOne(countSql, param);
+		page.setTotalCount(totalCount);
+		return page;
+	}
+}
---
+++ youxue-core/src/main/java/com/youxue/core/vo/Page.java
@@ -0,0 +1,127 @@
+package com.youxue.core.vo;
+
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.List;
+
+/***
+ * 
+ * @author lkzlee
+ *
+ * @param <T>
+ *
+ * 2016年11月7日
+ */
+@Deprecated
+public class Page<T> implements Serializable
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2023727607665537363L;
+
+	public static final int DEFAULT_PAGESIZE = 10;
+
+	public static final int SIX_PAGESIZE = 6;
+
+	protected List<T> result = Collections.emptyList();
+	protected long totalCount = -1;
+	protected int pageNo = 1;
+	protected int totalPage = 1;
+	protected int pageSize;
+
+	public Page()
+	{
+		super();
+		this.pageSize = DEFAULT_PAGESIZE;
+	}
+
+	public Page(int pageNo, int pageSize)
+	{
+		super();
+		this.pageNo = pageNo;
+		this.pageSize = pageSize;
+	}
+
+	public int getPageSize()
+	{
+		return pageSize;
+	}
+
+	public Page(int pageSize)
+	{
+		this.pageSize = pageSize;
+	}
+
+	public void setPageSize(int pageSize)
+	{
+		this.pageSize = pageSize;
+	}
+
+	public List<T> getResult()
+	{
+		return result;
+	}
+
+	public void setResult(List<T> result)
+	{
+		this.result = result;
+	}
+
+	public long getTotalCount()
+	{
+		return totalCount;
+	}
+
+	public void setTotalCount(int totalCount)
+	{
+		this.totalCount = totalCount;
+		if (totalCount == 0)
+		{
+			this.totalPage = 0;
+		}
+		else
+		{
+			this.totalPage = totalCount % this.getPageSize() == 0 ? totalCount / this.getPageSize() : totalCount
+					/ this.getPageSize() + 1;
+		}
+	}
+
+	public int getPageNo()
+	{
+		if (pageNo <= 0)
+		{
+			pageNo = 1;
+		}
+		return pageNo;
+	}
+
+	public void setPageNo(int pageNo)
+	{
+		this.pageNo = pageNo;
+	}
+
+	public int getTotalPage()
+	{
+		return totalPage;
+	}
+
+	public void setTotalPage(int totalPage)
+	{
+		this.totalPage = totalPage;
+	}
+
+	public void pageNoPlus(int i)
+	{
+		this.pageNo += i;
+	}
+
+	public static <T> Page<T> getInstantce(int pageNo, int pageSize, Class<T> c)
+	{
+		Page<T> page = new Page<>();
+		page.setPageSize(pageSize);
+		page.setPageNo(pageNo);
+		return page;
+	}
+
+}

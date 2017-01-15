22a3406425fbde386e29c02aaee180d6d24c0d19
Masterwind <zhangwei2014@corp.netease.com>
2017-01-15 14:58:56 +0800

    ueditor

--- youxue-admin/src/main/java/com/youxue/admin/power/constant/PowerConstant.java
+++ youxue-admin/src/main/java/com/youxue/admin/power/constant/PowerConstant.java
@@ -45,6 +45,8 @@ public class PowerConstant
 		tyGLMenu.getSonList().add(new MenuShow("搜索管理", "/searchList.do"));
 		tyGLMenu.getSonList().add(new MenuShow("关于我们", "/aboutUs.do"));
 		tyGLMenu.getSonList().add(new MenuShow("资讯", "/news.do"));
+		tyGLMenu.getSonList().add(new MenuShow("私人订制-定制案例", "/personal.do"));
+		tyGLMenu.getSonList().add(new MenuShow("私人订制-周边产品", "/surround.do"));
 		menuMap.put(PowerTypeEnum.TYGL.getValue(), Lists.newArrayList(tyGLMenu));
 		allMenuList.add(tyGLMenu);
 

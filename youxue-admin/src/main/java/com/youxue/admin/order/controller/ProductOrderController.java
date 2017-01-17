a0041a0b0aab00241d3e48bc4bb897871e93e5c4
liyongchao <liyongchao@corp.netease.com>
2017-01-17 16:29:48 +0800

    周边产品订单

---
+++ youxue-admin/src/main/java/com/youxue/admin/order/controller/ProductOrderController.java
@@ -0,0 +1,120 @@
+package com.youxue.admin.order.controller;
+
+import java.util.Date;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.lkzlee.pay.utils.DateUtil;
+import com.youxue.admin.order.constant.ConstantMapUtil;
+import com.youxue.core.common.BaseController;
+import com.youxue.core.dao.ProductOrderVoDao;
+import com.youxue.core.enums.PayTypeEnum;
+import com.youxue.core.vo.Page;
+import com.youxue.core.vo.ProductOrderVo;
+
+/***
+ * 产品订单页
+ * @author lkzlee
+ *
+ */
+@Controller
+public class ProductOrderController extends BaseController
+{
+	private final static Log LOG = LogFactory.getLog(ProductOrderController.class);
+	@Resource
+	private ProductOrderVoDao productOrderDao;
+
+	/***
+	 * 
+	 * @param request
+	 * @param response
+	 * @param orderId
+	 * @param accountId
+	 * @param payType
+	 * @param startTime
+	 * @param endTime
+	 * @param pageNo
+	 * @param modelMap
+	 * @return
+	 */
+	@RequestMapping(path = "/admin/productorder.do")
+	public String userPageOrderInfo(HttpServletRequest request, HttpServletResponse response, String orderId,
+			String accountId, String payType, String startTime, String endTime, String pageNo, ModelMap modelMap)
+	{
+		LOG.info("查询周边产品订单页 orderId=" + orderId + ",accountId=" + accountId + ",payType=" + payType + ",startTime="
+				+ startTime + ",endTime=" + endTime + ",pageNo=" + pageNo);
+		PayTypeEnum pType = PayTypeEnum.UNKNOW_PAY;
+		Date stTime = null;
+		Date edTime = null;
+		try
+		{
+			if (StringUtils.isNotBlank(payType))
+			{
+				int tmpType = Integer.parseInt(payType);
+				pType = PayTypeEnum.getByValue(tmpType);
+			}
+			if (StringUtils.isNotBlank(startTime))
+			{
+				stTime = DateUtil.formatToDate(startTime, DateUtil.DATE_FORMAT_YYYYMMDD_HHMMSS);
+			}
+			if (StringUtils.isNotBlank(endTime))
+			{
+				edTime = DateUtil.formatToDate(endTime, DateUtil.DATE_FORMAT_YYYYMMDD_HHMMSS);
+			}
+		}
+		catch (Exception e)
+		{
+		}
+
+		int pNum = Page.getPageNo(pageNo);
+		Page<ProductOrderVo> page = new Page<ProductOrderVo>(pNum, Page.DEFAULT_PAGESIZE);
+
+		page = productOrderDao.selectPageOrderListByInfo(page, pType, orderId, accountId, stTime, edTime);
+
+		LOG.info("查询周边订单页的结果为：resultList=" + page.getResultList());
+		modelMap.put("page", page);
+		modelMap.put("orderId", orderId);
+		modelMap.put("accountId", accountId);
+		modelMap.put("payType", pType.getValue());
+		modelMap.put("startTime", startTime);
+		modelMap.put("endTime", endTime);
+		modelMap.put("payTypeMap", ConstantMapUtil.payTypeMap);
+		return "/productOrder/productOrderList";
+
+	}
+
+	@RequestMapping(path = "/admin/addproductorder.do")
+	public String userPageOrderInfo(HttpServletRequest request, HttpServletResponse response,
+			ProductOrderVo productOrder)
+	{
+		LOG.info("插入产品订单操作 productOrder=" + productOrder);
+
+		try
+		{
+			checkParam(productOrder);
+			productOrder.setCreateTime(DateUtil.getCurrentTimestamp());
+			productOrderDao.insertSelective(productOrder);
+		}
+		catch (Exception e)
+		{
+		}
+
+		return "redirect:/admin/productorder.do";
+
+	}
+
+	private void checkParam(ProductOrderVo productOrder)
+	{
+		// TODO Auto-generated method stub
+
+	}
+}
---
+++ youxue-core/src/main/java/com/youxue/core/vo/ProductOrderVo.java
@@ -0,0 +1,96 @@
+package com.youxue.core.vo;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class ProductOrderVo {
+    private String orderId;
+
+    private Integer buyType;
+
+    private String content;
+
+    private Integer payType;
+
+    private String accountId;
+
+    private Date createTime;
+
+    private Date buyTime;
+
+    private BigDecimal price;
+
+    private String remark;
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId == null ? null : orderId.trim();
+    }
+
+    public Integer getBuyType() {
+        return buyType;
+    }
+
+    public void setBuyType(Integer buyType) {
+        this.buyType = buyType;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content == null ? null : content.trim();
+    }
+
+    public Integer getPayType() {
+        return payType;
+    }
+
+    public void setPayType(Integer payType) {
+        this.payType = payType;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId == null ? null : accountId.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getBuyTime() {
+        return buyTime;
+    }
+
+    public void setBuyTime(Date buyTime) {
+        this.buyTime = buyTime;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark == null ? null : remark.trim();
+    }
+}

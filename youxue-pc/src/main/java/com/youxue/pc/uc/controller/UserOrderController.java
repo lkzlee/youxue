fc814b7a0c637d8f81be7d94d1b5b04c3e939601
liyongchao <liyongchao@corp.netease.com>
2017-03-17 11:48:16 +0800

    订单删除兼容

--- youxue-pc/src/main/java/com/youxue/pc/uc/controller/UserOrderController.java
+++ youxue-pc/src/main/java/com/youxue/pc/uc/controller/UserOrderController.java
@@ -120,38 +120,48 @@ public class UserOrderController extends BaseController
 
 	@RequestMapping(path = "/uc/deleteorder.do")
 	@ResponseBody
-	public String deleteOrderInfo(HttpServletRequest request, HttpServletResponse response, String logicOrderId)
+	public String deleteOrderInfo(HttpServletRequest request, HttpServletResponse response, String logicOrderId,
+			String orderId)
 	{
 		String accountId = getCurrentLoginUserName(request);
-		LOG.info("删除订单，accountId=" + accountId + ",logicOrderId=" + logicOrderId);
+		LOG.info("删除订单，accountId=" + accountId + ",logicOrderId=" + logicOrderId + ",orderId=" + orderId);
 		if (StringUtils.isBlank(accountId))
 			return JsonUtil.serialize(BaseResponseDto.notLoginDto());
-		if (StringUtils.isBlank(logicOrderId))
+		if (StringUtils.isBlank(logicOrderId) && StringUtils.isBlank(orderId))
 		{
 			return JsonUtil.serialize(BaseResponseDto.errorDto().setDesc("参数非法"));
 		}
-		List<OrderVo> orderList = orderDao.selectOrderByLogicOrderId(logicOrderId, false);
-		if (orderList == null || CollectionUtils.isEmpty(orderList))
-		{
-			return JsonUtil.serialize(BaseResponseDto.errorDto().setDesc("订单不存在"));
-		}
 		int success = 0;
-		for (OrderVo order : orderList)
+		if (!StringUtils.isBlank(logicOrderId))
 		{
+			List<OrderVo> orderList = orderDao.selectOrderByLogicOrderId(logicOrderId, false);
+			if (orderList == null || CollectionUtils.isEmpty(orderList))
+			{
+				return JsonUtil.serialize(BaseResponseDto.errorDto().setDesc("订单不存在"));
+			}
+
+			for (OrderVo order : orderList)
+			{
+				if (!order.getAccountId().equalsIgnoreCase(accountId))
+				{
+					return JsonUtil.serialize(BaseResponseDto.errorDto().setDesc("订单不存在"));
+				}
+				order.setStatus(OrderVo.DELETED);
+				order.setUpdateTime(DateUtil.getCurrentTimestamp());
+				success += orderDao.updateByPrimaryKeySelective(order);
+			}
+		}
+		else if (!StringUtils.isBlank(orderId))
+		{
+			OrderVo order = orderDao.selectByPrimaryKey(orderId, false);
 			if (!order.getAccountId().equalsIgnoreCase(accountId))
 			{
 				return JsonUtil.serialize(BaseResponseDto.errorDto().setDesc("订单不存在"));
 			}
 			order.setStatus(OrderVo.DELETED);
 			order.setUpdateTime(DateUtil.getCurrentTimestamp());
-			success += orderDao.updateByPrimaryKeySelective(order);
+			success = orderDao.updateByPrimaryKeySelective(order);
 		}
-		//		if (order.getStatus() != OrderVo.UNPAY && order.getStatus() != OrderVo.DELETED
-		//				&& order.getStatus() != OrderVo.DONE)
-		//		{
-		//			return JsonUtil.serialize(BaseResponseDto.errorDto().setDesc("订单状态不正确，不能删除该订单"));
-		//		}
-
 		if (success > 0)
 			return JsonUtil.serialize(BaseResponseDto.successDto().setDesc("删除成功"));
 		return JsonUtil.serialize(BaseResponseDto.errorDto().setDesc("删除失败"));
